ceab990a7f37c7c92bd0c36f898d84ebc090fb17
Komal Sinha <komalsinha1811@gmail.com>
2025-12-24 16:09:32 +0530

    Add Kinde Auth setup including login, logout, and callback handling

--- app/(routes)/_common/header.tsx
+++ app/(routes)/_common/header.tsx
@@ -2,12 +2,17 @@
 import Logo from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { MoonIcon, SunIcon } from "lucide-react";
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { LogOutIcon, MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Header = () => {
   const { theme, setTheme } = useTheme();
+  const {user, getUser} = useKindeBrowserClient();
   const isDark = theme === "dark";
   return (
     <div className="sticky top-0 right-0 left-0 z-30">
@@ -49,7 +54,39 @@ const Header = () => {
                 )}
               />
             </Button>
-             <Button>Sign in</Button>
+              {user ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar
+                    className="h-8 w-8
+                  shrink-0 rounded-full"
+                  >
+                    <AvatarImage
+                      src={user?.picture || ""}
+                      alt={user?.given_name || ""}
+                    />
+                    <AvatarFallback className="rounded-lg">
+                      {user?.given_name?.charAt(0)}
+                      {user?.family_name?.charAt(0)}
+                    </AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <LogoutLink className="w-full flex items-center">
+                      <LogOutIcon className="size-4" />
+                      Logout
+                    </LogoutLink>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ) : (
+              <LoginLink>
+                <Button>Sign in</Button>
+              </LoginLink>
+            )}
           </div>
         </div>
       </header>
